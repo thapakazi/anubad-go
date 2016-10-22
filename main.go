468831db5e66b090d20b967fb476744f3ca37522
Milan Thapa <milans.thapa78@gmail.com>
2016-10-22 21:49:36 +0545

    adding basic template

--- main.go
+++ main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func main() {
+type Anubad struct {
+	Word    string
+	Meaning string
+}
 
+func main() {
+	templates := template.Must(template.ParseFiles("templates/index.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, नमस्ते everyone... this is how api.anubad.io begins")
+		anubad := Anubad{Word: "Dictonary", Meaning: "शब्दकोष"}
+		if word := r.FormValue("sabda"); word != "" {
+			anubad.Word = word
+		}
+		if err := templates.ExecuteTemplate(w, "index.html", anubad); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 
 	})
 	fmt.Println(http.ListenAndServe(":2048", nil))
